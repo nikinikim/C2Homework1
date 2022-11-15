d6beb10c75a533dbcc7bcebc2e4da26d3ffe4643
Николай <colt92@inbox.ru>
2022-11-15 21:17:01 +0300

    Выполнены 1 и 2 задания ДЗ

--- Main.java
+++ Main.java
@@ -1,2 +1,13 @@
 public class Main {
+    public static void main(String[] args) {
+        //Task1
+        Human maksim = new Human("Максим", "Минск", "бренд-менеджер",1988);
+        Human anya = new Human("Аня", "Москва","методист образовательных программ" ,1993);
+        Human katya = new Human("Катя", "Калининград","продакт-менеджер", 1992);
+        Human artem = new Human("Артем", "Москва","директор по развитию бизнеса", 1995);
+        System.out.println(maksim);
+        System.out.println(anya);
+        System.out.println(katya);
+        System.out.println(artem);
+    }
 }
